8cfd3d802ae1fcff8cdb7c5d1c9802339085c2c9
MahmudJion <mahmudh.jion02@gmail.com>
2024-10-18 07:48:40 +0100

    sum of arrays

---
+++ sum_of_arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << "Sum of array elements: " << sum << endl;
+
+    return 0;
+}
